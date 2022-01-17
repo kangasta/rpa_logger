eeb1a0a3a33487ef99260d21938ce6f223351c14
Toni Kangas <toni.ar.kangas@gmail.com>
2022-01-17 02:50:09 +0200

    Fix start and log task prints, add option to print output immediately

---
+++ examples/log_output.py
@@ -0,0 +1,17 @@
+from time import sleep
+from rpa_logger import Logger
+from rpa_logger.utils.args import get_argparser, get_rpa_logger_parameters
+
+args = get_argparser().parse_args()
+l = Logger(**get_rpa_logger_parameters(args))
+
+key = l.start_task('Log task output')
+sleep(1)
+
+l.log_output(key, 'Tasks often produce output.')
+sleep(1)
+
+l.log_output(key, 'Output can be logged to tasks.')
+sleep(1)
+
+l.finish_task('SUCCESS', key=key)
